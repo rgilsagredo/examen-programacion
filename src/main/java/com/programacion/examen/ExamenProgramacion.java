6ea3c2671d8dc5c0ca1525ac8adb4233e7ddba3a
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-05 11:15:42 +0100

    limpieza de código. Elimino test manuales. Listo para merge

--- src/main/java/com/programacion/examen/ExamenProgramacion.java
+++ src/main/java/com/programacion/examen/ExamenProgramacion.java
@@ -11,56 +11,6 @@ public class ExamenProgramacion {
         final int TAMAÑ0 = 10;
         final char[] TABLERO = new char[TAMAÑ0];
 
-        // añado tableros "test" a mano para probar que mi metodo de victoria funciona
-        final char[] T1 = new char[TAMAÑ0];
-        final char[] T2 = new char[TAMAÑ0];
-        final char[] T3 = new char[TAMAÑ0];
-        final char[] T4 = new char[TAMAÑ0];
-
-        // T1 -- victoria en un extremo
-        T1[0] = ORDENADOR;
-        T1[1] = ORDENADOR;
-        T1[2] = ORDENADOR;
-
-        // T2 -- victoria en medio
-        T2[3] = USUARIO;
-        T2[4] = USUARIO;
-        T2[5] = USUARIO;
-
-        // T3 -- victoria en otro extremo
-        T3[9] = ORDENADOR;
-        T3[8] = ORDENADOR;
-        T3[7] = ORDENADOR;
-
-        // T4 -- sin victoria, con huecos
-        T4[0] = ORDENADOR;
-        T4[1] = USUARIO;
-        T4[2] = ORDENADOR;
-        T4[3] = ORDENADOR;
-        T4[6] = USUARIO;
-        T4[9] = USUARIO;
-
-        // T5 -- sin victoria, sin huecos
-        final char[] T5 = { USUARIO, ORDENADOR, USUARIO, USUARIO, ORDENADOR, USUARIO, USUARIO, ORDENADOR, USUARIO,
-                USUARIO };
-
-        // victoria = encontrar 3 iguales seguidas
-        // recorer tablero hasta TAMAÑO-2, ver posicion actual, si esa es igual a las 2
-        // siguientes, hay ganador
-        boolean hayGanador = hayGanador(T1, TAMAÑ0);
-        System.out.println("T1: " + hayGanador);
-
-        hayGanador = hayGanador(T2, TAMAÑ0);
-        System.out.println("T2: " + hayGanador);
-
-        hayGanador = hayGanador(T3, TAMAÑ0);
-        System.out.println("T3: " + hayGanador);
-
-        hayGanador = hayGanador(T4, TAMAÑ0);
-        System.out.println("T4: " + hayGanador);
-
-        hayGanador = hayGanador(T5, TAMAÑ0);
-        System.out.println("T5: " + hayGanador);
 
     } // main
 
@@ -76,5 +26,5 @@ public class ExamenProgramacion {
         } // recorrer tablero
 
         return hayGanador;
-    }
+    } // hayGanador
 } // ExamenProgramacion
