77aaf1e38c69489d62374b8cc02274c2e5e525dd
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-05 12:13:46 +0100

    testeado, bug resuelto

--- src/main/java/com/programacion/examen/ExamenProgramacion.java
+++ src/main/java/com/programacion/examen/ExamenProgramacion.java
@@ -22,7 +22,8 @@ public class ExamenProgramacion {
         T1[9] = USUARIO;
 
         // T2 - hay ganador sin huecos
-        final char[] T2 = {ORDENADOR,ORDENADOR,USUARIO,USUARIO,USUARIO,ORDENADOR,USUARIO,ORDENADOR,USUARIO,ORDENADOR};
+        final char[] T2 = { ORDENADOR, ORDENADOR, USUARIO, USUARIO, USUARIO, ORDENADOR, USUARIO, ORDENADOR, USUARIO,
+                ORDENADOR };
 
         // T3 - no hay ganador con huecos
         final char[] T3 = new char[TAMAÑ0];
@@ -33,23 +34,22 @@ public class ExamenProgramacion {
         T3[9] = USUARIO;
 
         // T4 - no hay ganador sin huecos
-        final char[] T4 = {ORDENADOR,ORDENADOR,USUARIO,ORDENADOR,USUARIO,ORDENADOR,USUARIO,ORDENADOR,USUARIO,ORDENADOR};
+        final char[] T4 = { ORDENADOR, ORDENADOR, USUARIO, ORDENADOR, USUARIO, ORDENADOR, USUARIO, ORDENADOR, USUARIO,
+                ORDENADOR };
 
         boolean hayGanador;
 
-        hayGanador = hayGanador(T1,TAMAÑ0,ORDENADOR,USUARIO);
+        hayGanador = hayGanador(T1, TAMAÑ0, ORDENADOR, USUARIO);
         System.out.println("T1: " + hayGanador);
 
-        hayGanador = hayGanador(T2,TAMAÑ0,ORDENADOR,USUARIO);
+        hayGanador = hayGanador(T2, TAMAÑ0, ORDENADOR, USUARIO);
         System.out.println("T2: " + hayGanador);
 
-        hayGanador = hayGanador(T3,TAMAÑ0,ORDENADOR,USUARIO);
+        hayGanador = hayGanador(T3, TAMAÑ0, ORDENADOR, USUARIO);
         System.out.println("T3: " + hayGanador);
 
-        hayGanador = hayGanador(T4,TAMAÑ0,ORDENADOR,USUARIO);
+        hayGanador = hayGanador(T4, TAMAÑ0, ORDENADOR, USUARIO);
         System.out.println("T4: " + hayGanador);
-
-
 
     } // main
 
@@ -58,7 +58,7 @@ public class ExamenProgramacion {
         char posicionActual;
         for (int i = 0; i < TAMAÑ0 - 2; i++) {
             posicionActual = TABLERO[i];
-            if (posicionActual != ORDENADOR || posicionActual != USUARIO) {
+            if (posicionActual != ORDENADOR && posicionActual != USUARIO) {
                 continue;
             }
             if (TABLERO[i + 1] == posicionActual && TABLERO[i + 2] == posicionActual) {
