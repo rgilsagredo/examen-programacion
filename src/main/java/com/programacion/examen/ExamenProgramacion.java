e2b3bcd8e8a73eea974b53e47230b8ef14631ef3
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-05 12:37:30 +0100

    codigo limpiado. Metodo hayEmpate OK

--- src/main/java/com/programacion/examen/ExamenProgramacion.java
+++ src/main/java/com/programacion/examen/ExamenProgramacion.java
@@ -11,52 +11,11 @@ public class ExamenProgramacion {
         final int TAMAÑ0 = 10;
         final char[] TABLERO = new char[TAMAÑ0];
 
-        // empate = no hay ganador y no hay huecos
-        // creamos arrays para testeo
-
-        // T1 -- hay ganador con huecos
-        final char[] T1 = new char[TAMAÑ0];
-        T1[2] = USUARIO;
-        T1[3] = USUARIO;
-        T1[4] = USUARIO;
-        T1[0] = ORDENADOR;
-        T1[9] = ORDENADOR;
-
-        // T2 -- hay ganador sin huecos
-        final char[] T2 = { USUARIO, USUARIO, ORDENADOR, ORDENADOR, ORDENADOR, USUARIO, ORDENADOR, USUARIO, ORDENADOR,
-                USUARIO };
-
-        // T3 -- no hay ganador con huecos
-        final char[] T3 = new char[TAMAÑ0];
-        T3[2] = USUARIO;
-        T3[3] = ORDENADOR;
-        T3[4] = USUARIO;
-        T3[0] = ORDENADOR;
-        T3[9] = ORDENADOR;
-
-        // T4 -- no hay ganador sin huecos
-        final char[] T4 = { USUARIO, USUARIO, ORDENADOR, USUARIO, ORDENADOR, USUARIO, ORDENADOR, USUARIO, ORDENADOR,
-                USUARIO };
-
-        // detectamos empates
-        boolean hayEmpate = hayEmpate(ORDENADOR, USUARIO, TAMAÑ0, TABLERO);
-        System.out.println("T1: " + hayEmpate);
-
-         
-        hayEmpate = hayEmpate(ORDENADOR, USUARIO, TAMAÑ0, T2);
-        System.out.println("T2: " + hayEmpate);
-
-        hayEmpate = hayEmpate(ORDENADOR, USUARIO, TAMAÑ0, T3);
-        System.out.println("T3: " + hayEmpate);
-
-        hayEmpate = hayEmpate(ORDENADOR, USUARIO, TAMAÑ0, T4);
-        System.out.println("T4: " + hayEmpate);
-
     } // main
 
     private static boolean hayEmpate(final char ORDENADOR, final char USUARIO, final int TAMAÑ0, final char[] TABLERO) {
         boolean hayEmpate = false;
-        if(!hayGanador(TABLERO, TAMAÑ0, ORDENADOR, USUARIO) && !hayHuecos(ORDENADOR, USUARIO, TABLERO)){
+        if (!hayGanador(TABLERO, TAMAÑ0, ORDENADOR, USUARIO) && !hayHuecos(ORDENADOR, USUARIO, TABLERO)) {
             hayEmpate = true;
         }
         return hayEmpate;
