8ec30a55560e7999d12e85fa65845606fbef1e46
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-05 12:03:07 +0100

    detectado posible bug en hayGanador. Solucion hecha, PDTE de testeo

--- src/main/java/com/programacion/examen/ExamenProgramacion.java
+++ src/main/java/com/programacion/examen/ExamenProgramacion.java
@@ -14,11 +14,14 @@ public class ExamenProgramacion {
 
     } // main
 
-    public static boolean hayGanador(final char[] TABLERO, final int TAMAÑ0) {
+    public static boolean hayGanador(final char[] TABLERO, final int TAMAÑ0, final char ORDENADOR, final char USUARIO) {
         boolean hayGanador = false;
         char posicionActual;
         for (int i = 0; i < TAMAÑ0 - 2; i++) {
             posicionActual = TABLERO[i];
+            if(posicionActual != ORDENADOR || posicionActual != USUARIO){
+                continue;
+            }
             if (TABLERO[i + 1] == posicionActual && TABLERO[i + 2] == posicionActual) {
                 hayGanador = true;
                 break;
