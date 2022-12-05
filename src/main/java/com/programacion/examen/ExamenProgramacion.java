b865d316ac3ab2c30f3b8758108405a6b95f56ff
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-05 10:38:30 +0100

    merge dev/crear-tablero con main

--- src/main/java/com/programacion/examen/ExamenProgramacion.java
+++ src/main/java/com/programacion/examen/ExamenProgramacion.java
@@ -1,15 +1,16 @@
 package com.programacion.examen;
 
-/**
- * Hello world!
- *
- */
+
 public class ExamenProgramacion {
     public static void main(String[] args) {
 
         // creamos jugadores
         final char ORDENADOR = 'O';
         final char USUARIO = 'U';
+
+        // creamos tablero
+        final int TAMAÑ0 = 10; 
+        final char[] TABLERO = new char[TAMAÑ0];
 
     } // main
 } // ExamenProgramacion
